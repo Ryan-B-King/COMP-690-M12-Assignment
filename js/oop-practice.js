4751e20945c183c1246a6729b431a7a81ba3cb93
Ryan King <33941531+Ryan-B-King@users.noreply.github.com>
2023-05-16 12:14:22 -0700

    assignment completed

--- js/oop-practice.js
+++ js/oop-practice.js
@@ -1,17 +1,98 @@
 // STEP 1
 
+// function Cat() {}
+
+// const cat = new Cat();
+// const dog = new function () {};
+
 // STEP 2
+// const cat2 = new Cat();
+// const dog2 = new function () {};
 
 // STEP 3
+// function Animal() {}
+// const animal = new Animal();
+// animal.createAnimal = () => console.log('The Animal has been created');
+// animal.createAnimal();
 
 // STEP 4
+// function Animal(message) {
+//   this.message = message;
+//   console.log(message);
+// }
+// const animal = new Animal('The Animal has been created');
 
 // STEP 5
+// function Animal(type, breed, color, height, length) {
+//   this.type = type;
+//   this.breed = breed;
+//   this.color = color;
+//   this.height = height;
+//   this.length = length;
+// }
+// const animal = new Animal('Dog', 'Chihuahua', 'Brown', '1ft', '1ft 5in');
 
 // STEP 6
+// for (property in animal) {
+//   console.log(property);
+// }
 
 // STEP 7
 
+// function Animal(type, breed, color, height, length) {
+//   this.type = type;
+//   this.breed = breed;
+//   this.color = color;
+//   this.height = height;
+//   this.length = length;
+
+//   this.speak = () => {
+//     const type = this.type.toLowerCase();
+//     const color = this.color.toLowerCase();
+//     if (type === 'dog') {
+//       console.log(`The ${color} dog is barking!`);
+//     } else if (type === 'cat') {
+//       console.log(`The ${color} cat is meowing!`);
+//     }
+//   };
+// }
+// const animal = new Animal('Cat', 'Calico', 'Orange', '1ft 3in', '1ft 5in');
+// animal.speak();
+
 // STEP 8
 
+// function Animal(type, breed, color, height, length) {
+//   const _type = type;
+//   const _breed = breed;
+//   const _color = color;
+//   const _height = height;
+//   const _length = length;
+
+//   const checkType = () => {
+//     if (_type.toLowerCase() === 'dog') {
+//       return 'dog';
+//     } else if (_type.toLowerCase() === 'cat') {
+//       return 'cat';
+//     }
+//   };
+
+//   this.speak = () => {
+//     console.log(`The ${checkType()} has made a noise!`);
+//   };
+// }
+// const animal = new Animal('Cat', 'Calico', 'Orange', '1ft 3in', '1ft 5in');
+// animal.speak();
+
 // STEP 9
+// String.prototype.findWords = (input, word) => {
+//   window.alert(
+//     `The word ${word} was found ${
+//       Array.from(input.matchAll(word)).length
+//     } times!`
+//   );
+// };
+
+// const inp =
+//   'Dolore shank duis prosciutto sausage ut nostrud ex pork chop pork loin cow eiusmod chuck brisket shankle. Ipsum ut eu fugiat, ut ball tip shankle short loin spare ribs ham shoulder. Mollit pork loin drumstick, in proident strip steak cupim picanha kielbasa adipisicing cillum enim cow short loin. Shankle turkey commodo ex voluptate, magna incididunt.';
+
+// String.prototype.findWords(inp, 'pork');
--- js/player.js
+++ js/player.js
@@ -1,51 +1,72 @@
 class Jukebox {
-    constructor(albums) {
-        this.albums = []
+  constructor(albums) {
+    this.albums = [];
+  }
+  addAlbum = function (album) {
+    this.albums.push(album);
+  };
+  favoriteAlbum = function () {
+    let max = -1,
+      fav;
+    for (let i = 0; i < this.albums.length; i++) {
+      if (this.albums[i].played > max) {
+        max = this.albums[i].played;
+        fav = this.albums[i];
+      }
     }
-    addAlbum = function(album) {
-        this.albums.push(album)
-    }
-    favoriteAlbum = function() {
-        let max = -1, fav
-        for (let i = 0; i < this.albums.length; i++) {
-            if (this.albums[i].played > max) {
-                max = this.albums[i].played
-                fav = this.albums[i]
-            }
-        }
-        return fav.display()
-    }
+    return fav.display();
+  };
 }
 
 class Album {
-    constructor(artist, title) {
-        this.artist = artist
-        this.title = title
-        this.played = 0
-    }
-    play = function() {
-        this.played += 1
-    }
-    display = function() {
-        return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
-    }
+  constructor(artist, title) {
+    this.artist = artist;
+    this.title = title;
+    this.played = 0;
+  }
+  play = function () {
+    this.played += 1;
+  };
+  display = function () {
+    return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`;
+  };
 }
 
-var jbox = new Jukebox()
-const album1 = new Album('Operation Ivy', 'Energy')
-const album2 = new Album('Blink 182', 'Dude Ranch')
-const album3 = new Album('New Found Glory', 'Sticks and Stones')
+var jbox = new Jukebox();
+const albums = [
+  ['Operation Ivy', 'Energy'],
+  ['Blink 182', 'Dude Ranch'],
+  ['New Found Glory', 'Sticks and Stones'],
+];
 
-jbox.addAlbum(album1)
-jbox.addAlbum(album2)
-jbox.addAlbum(album3)
+window.onload = function () {
+  const favoriteAlbumButton = document.getElementById('favoriteAlbumButton');
 
-album1.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album2.play()
-album3.play()
+  favoriteAlbumButton.addEventListener('click', () => {
+    const favoriteAlbum = document.createTextNode(jbox.favoriteAlbum());
+    document.getElementById('favoriteAlbum').innerHTML = '';
+    document.getElementById('favoriteAlbum').appendChild(favoriteAlbum);
+  });
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+  albums.forEach((album) => {
+    const option = document.createElement('option');
+    const jukeboxAlbum = new Album(album[0], album[1]);
+    const albumText = `${jukeboxAlbum.title} - ${jukeboxAlbum.artist}`;
+
+    const text = document.createTextNode(albumText);
+    1;
+    option.value = albumText;
+    option.appendChild(text);
+
+    jbox.addAlbum(jukeboxAlbum);
+
+    document.getElementById('albumSelect').append(option);
+  });
+
+  const playButton = document.getElementById('playButton');
+  playButton.addEventListener('click', (e) => {
+    const index =
+      document.getElementById('albumSelect').selectedOptions[0].index;
+    jbox.albums[index].play();
+  });
+};
